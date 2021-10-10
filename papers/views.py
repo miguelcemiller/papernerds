8a0542430076e581f853d2122e3cfd15a2aa9472
Miguel Cemiller <miguelcemiller@gmail.com>
2021-10-10 17:25:13 +0800

    improved search

--- papers/views.py
+++ papers/views.py
@@ -1,45 +1,29 @@
 from django.shortcuts import render
 from . models import Paper
+from django.db.models import Q
 
 from . boyermoore import boyer_moore_horspool
 
+
 def homeView(request):
 
-    paper_indexes_list = []
-    is_search = False
+    search = ""
 
     if 'search' in request.GET:
-        is_search = True
-
         search = request.GET['search']
-        papers = Paper.objects.all().filter(abstract__icontains=search) #filter papers first based on abstract
+        #papers = Paper.objects.all().filter(abstract__icontains=search) #filter papers first based on abstract # only works with one word or 2 words together
 
-        search_len = len(search) # length of search
- 
-        for paper in papers: # loop through each papers
-            abstract = paper.abstract # get the abstract
-            results = boyer_moore_horspool(abstract, search) # this will return all starting indexes from abstract
+        q = None
+        for word in search.split():
+            q_aux = Q(title__icontains = word) | Q(abstract__icontains = word)
+            q = ( q_aux & q ) if bool( q ) else q_aux
 
-            indexes = []
-
-            for start_index in results: # loop through each starting indexes
-                end_index = start_index + search_len # get last index
-
-                start_end_index = []   # [4, 7]
-                start_end_index.extend((start_index, end_index)) # [4, 7]
-
-                indexes.append(start_end_index)
+        papers = Paper.objects.filter(q) 
         
-            paper_indexes_list.append(indexes)
-        
-        
-        print(paper_indexes_list[0])
-
     else:
         papers = Paper.objects.all()
 
-
-    context = {'papers': papers, 'indexes': paper_indexes_list, 'is_search': is_search }
+    context = {'papers': papers, 'search': search }
     return render(request, 'papers/home.html', context)
 
 
