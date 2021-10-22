9ed4f7c16b847aa7d7f9177d299bc575c8f6a7ce
Miguel Cemiller <miguelcemiller@gmail.com>
2021-10-22 18:38:35 +0800

    integrated tf idf

---
+++ papers/tf_idf.py
@@ -0,0 +1,64 @@
+import os
+import re
+import time
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity 
+
+
+
+# Define the Functions
+
+def preprocess(body):
+    """ Preprocess the input, i.e. lowercase, remove html tags, special character and digits."""
+    
+    text = body
+    
+    # to lower case
+    text = text.lower()
+
+    # remove tags
+    text = re.sub("</?.*?>"," <> ", text)
+
+
+    # remove special characters and digits
+    text = re.sub("(\\d|\\W)+"," ", text).strip()
+    return text
+
+def create_tfidf_features(corpus, max_features=5000, max_df=0.95, min_df=2):
+    """ Creates a tf-idf matrix for the `corpus` using sklearn. """
+    tfidf_vectorizor = TfidfVectorizer(decode_error='replace', strip_accents='unicode', analyzer='word',
+                                       stop_words='english', ngram_range=(1, 1), max_features=max_features,
+                                       norm='l2', use_idf=True, smooth_idf=True, sublinear_tf=True,
+                                       max_df=max_df, min_df=min_df)
+    X = tfidf_vectorizor.fit_transform(corpus)
+    print('tfidf matrix successfully created.')
+    return X, tfidf_vectorizor
+
+def calculate_similarity(X, vectorizor, query, top_k=5):
+    """ Vectorizes the `query` via `vectorizor` and calculates the cosine similarity of
+    the `query` and `X` (all the documents) and returns the `top_k` similar documents."""
+
+    # Vectorize the query to the same length as documents
+    query_vec = vectorizor.transform(query)
+    # Compute the cosine similarity between query_vec and all the documents
+    cosine_similarities = cosine_similarity(X,query_vec).flatten()
+    # Sort the similar documents from the most similar to less similar and return the indices
+    most_similar_doc_indices = np.argsort(cosine_similarities, axis=0)[:-top_k-1:-1]
+    return (most_similar_doc_indices, cosine_similarities)
+
+
+def show_similar_documents(df, cosine_similarities, similar_doc_indices):
+    """ Prints the most similar documents using indices in the `similar_doc_indices` vector."""
+    counter = 1
+    for index in similar_doc_indices:
+        if str(cosine_similarities[index]) != '0.0':
+            print('Top-{}, Similarity = {}'.format(counter, cosine_similarities[index]))
+            print('body: {}, '.format(df[index]))
+            print()
+            counter += 1
+    return counter - 1 # to know how many index
+
+
+
+
--- papers/views.py
+++ papers/views.py
@@ -8,6 +8,11 @@ from django.db.models import F
 from collections import OrderedDict
 
 from . boyermoore import boyer_moore_horspool
+from . tf_idf import preprocess, create_tfidf_features, calculate_similarity, show_similar_documents
+
+import time
+
+import pandas as pd
 
 
 def homeView(request):
@@ -16,24 +21,103 @@ def homeView(request):
 
     if request.method == 'GET' and 'search' in request.GET:
         search = request.GET['search']
-        #papers = Paper.objects.all().filter(abstract__icontains=search) #filter papers first based on abstract # only works with one word or 2 words together
+        
         if search:
-            q = None
-            for word in search.split(" "):
-                # print(word)
-                q_aux = Q(title__icontains = word) | Q(abstract__icontains = word)
-                q = ( q_aux & q ) if bool( q ) else q_aux
+            #get the corpus
+            corpus_abstracts = Paper.objects.values_list('abstract', flat=True)
 
-            papers = Paper.objects.filter(q)
+            df = pd.DataFrame(corpus_abstracts, columns=["abstract"])
+
+            #preprocess the corpus
+            data = [preprocess(abstract) for abstract in df['abstract']]
+
+            # Learn vocabulary and idf, return term-document matrix
+            X,v = create_tfidf_features(data)
+            features = v.get_feature_names_out()
+
+            #run
+            user_question = [search]
+            search_start = time.time()
+            sim_vecs, cosine_similarities = calculate_similarity(X, v, user_question)
+            search_time = time.time() - search_start
+            print("search time: {:.2f} ms".format(search_time * 1000))
+            print()
+           
+
+           # show similary and documents
+            search_num = show_similar_documents(data, cosine_similarities, sim_vecs)
+
+            # to set list in order when filtered in model
+            def filter__in_preserve(queryset: QuerySet, field: str, values: list) -> QuerySet:
+                """
+                .filter(field__in=values), preserves order.
+                """
+                # (There are not going to be missing cases, so default=len(values) is unnecessary)
+                preserved = Case(*[When(**{field: val}, then=pos) for pos, val in enumerate(values)])
+                return queryset.filter(**{f'{field}__in': values}).order_by(preserved)
+
+
+            abstract_list = []
+
+            if search_num is not 0:
+                for i in cosine_similarities.argsort()[-search_num:][::-1]:
+                    #print(df.iloc[i, 0])
+                    abstract_list.append(df.iloc[i, 0])
+            
+            #papers = Paper.objects.filter(abstract__in=abstract_list)
+            papers = filter__in_preserve(Paper.objects, 'abstract', abstract_list).all()
+
         else:
-            papers = Paper.objects.all()  
+            # empty search
+            papers = Paper.objects.all()
+  
     else:
+        # no search
         papers = Paper.objects.all()
-
 
 
     context = {'papers': papers, 'search': search }
     return render(request, 'papers/home.html', context)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def homeView(request):
+
+#     search = ""
+
+#     if request.method == 'GET' and 'search' in request.GET:
+#         search = request.GET['search']
+#         #papers = Paper.objects.all().filter(abstract__icontains=search) #filter papers first based on abstract # only works with one word or 2 words together
+#         if search:
+#             q = None
+#             for word in search.split(" "):
+#                 # print(word)
+#                 q_aux = Q(title__icontains = word) | Q(abstract__icontains = word)
+#                 q = ( q_aux & q ) if bool( q ) else q_aux
+
+#             papers = Paper.objects.filter(q)
+#         else:
+#             papers = Paper.objects.all()  
+#     else:
+#         papers = Paper.objects.all()
+
+
+
+#     context = {'papers': papers, 'search': search }
+#     return render(request, 'papers/home.html', context)
 
 
 
