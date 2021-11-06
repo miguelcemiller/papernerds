5e51d211acf649106fc83369e93719c5a1d567a8
Miguel Cemiller <miguelcemiller@gmail.com>
2021-11-06 16:44:28 +0800

    fixed routing with trailing slash

--- features/urls.py
+++ features/urls.py
@@ -1,15 +1,17 @@
 from django.urls import path
+from django.shortcuts import redirect
+from django.views.generic import RedirectView
 from . import views
 
 urlpatterns = [
     path('', views.landingView, name='landing'),
     path('home', views.homeView, name='home'),
+    path('home/', RedirectView.as_view(pattern_name = 'home')),
     path('paper/<slug:slug>', views.paperView, name="paper"),
+    path('paper/<slug:slug>/', RedirectView.as_view(pattern_name = 'paper')),
 
     path('login/', views.loginView, name='login'),
     path('logout/', views.logoutView, name='logout'),
     path('results', views.resultsView, name='results'),
-    
 
-   
 ]
